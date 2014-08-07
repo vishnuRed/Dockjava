92c9fb887597100abb3bc2a105b8add09470abee
vishnuRed <vishnu.virtual@gmail.com>
2014-08-07 19:23:26 +0530

    added sample web applications

---
+++ FirstServlets/src/com/Servlet/hello/Hello.java
@@ -0,0 +1,39 @@
+package com.Servlet.hello;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.setContentType("text/hxml");
+		
+		PrintWriter printWriter=response.getWriter();
+		printWriter.println("<html><body>");
+		printWriter.println("welcome");
+		printWriter.println("</body></html>");
+		printWriter.close();
+	}
+
+}
